5f9a45ab332b0e38d0a8ae2ad84b266dc0f75173
Aisa Nicevic <aisanicevic04@gmail.com>
2022-04-30 16:54:39 +0200

    first

---
+++ commands/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Ping: require("./ping"),
+  ArgsInfo: require("./args-info"),
+  Help: require("./help"),
+  Commands: require("./commands"),
+  Add: require("./add"),
+  Greetings: require("./greetings"),
+};
